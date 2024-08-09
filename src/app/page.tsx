fec1e6d9d2c9b3996abdead05433e6ba13b4a796
Gadzhieva <minnigareevalr@gmail.com>
2024-08-09 22:39:02 +0100

    Created grid with desserts, added pictures, adjusted font and margin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,65 @@
+import Image from 'next/image';
 
+interface DessertItem {
+    name: string,
+    imageDesktop: string,
+}
 
 const Desserts = () => {
+
+    const dessertList: DessertItem[] = [
+        {
+            name: "Waffle with Berries",
+            imageDesktop: "/images/image-waffle-desktop.jpg",
+        },
+        {
+            name: "Vanilla Bean Crème Brûlée",
+            imageDesktop: "/images/image-creme-brulee-desktop.jpg",
+        },
+        {
+            name: "Macaron Mix of Five",
+            imageDesktop: "/images/image-macaron-desktop.jpg",
+        },
+        {
+            name: "Classic Tiramisu",
+            imageDesktop: "/images/image-tiramisu-desktop.jpg",
+        },
+        {
+            name: "Pistachio Baklava",
+            imageDesktop: "/images/image-baklava-desktop.jpg",
+        },
+        {
+            name: "Lemon Meringue Pie",
+            imageDesktop: "/images/image-meringue-desktop.jpg",
+        },
+        {
+            name: "Red Velvet Cake",
+            imageDesktop: "/images/image-cake-desktop.jpg",
+        },
+        {
+            name: "Salted Caramel Brownie",
+            imageDesktop: "/images/image-brownie-desktop.jpg",
+        },
+        {
+            name: "Vanilla Panna Cotta",
+            imageDesktop: "/images/image-panna-cotta-desktop.jpg",
+        }
+    ]
+
     return (
-        <div className="grid grid-cols-3 gap-4">
+        <div className="grid grid-cols-3 gap-4 m-20">
             <div className="col-span-2">
                 <h1>Desserts</h1>
+                <div className="grid grid-cols-3 gap-4 mt-5">
+                    {dessertList.map((dessert, i) => {
+                        return (
+                            <div key={i}>
+                                <Image width={200} height={200} src={dessert.imageDesktop}/>
+                                <p>{dessert.name}</p>
+                            </div>
+                        )
+                    })}
+                </div>
             </div>
             <div>
                 <h1>Your cart</h1>
